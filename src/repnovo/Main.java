70b86c3d2a7271c704799ed35d1b02a6ddcf110d
bormanico <97111633+bormanico@users.noreply.github.com>
2022-01-17 15:42:58 +0000

    Update Main.java
    Retirei a tua função mult e adicionei a função multiply

--- src/repnovo/Main.java
+++ src/repnovo/Main.java
@@ -1,27 +1,24 @@
 package repnovo;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!!");
-		
-		System.out.println(soma (1,1));
-		System.out.println(mult (3,3));
+
+		System.out.println(soma(1, 1));
+		System.out.println(sub(3, 3));
 	}
-	
-	public static int soma (int aNum1, int aNum2)
-	{
+
+	public static int soma(int aNum1, int aNum2) {
 		return aNum1 + aNum2;
 	}
 
-	public static int sub (int aNum1, int aNum2)
-	{
+	public static int sub(int aNum1, int aNum2) {
 		return aNum1 - aNum2;
 	}
-	
-	public static int mult (int aNum1, int aNum2)
-	{
-		return aNum1 * aNum2;
+
+	public static int multiply(int int1, int int2) {
+		int a = int1 * int2;
+		return a;
 	}
-	
 }
