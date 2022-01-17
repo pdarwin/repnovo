d1414eb0701837dcfc18236172b5ddeb3bd50f84
pdarwin <46503478+pdarwin@users.noreply.github.com>
2022-01-17 15:32:20 +0000

    adição da função soma e subtração

--- src/repnovo/Main.java
+++ src/repnovo/Main.java
@@ -1,9 +1,20 @@
 package repnovo;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!!!");
+		
+		System.out.println(Soma (1,1));
+	}
+	
+	public static int Soma (int aNum1, int aNum2)
+	{
+		return aNum1 + aNum2;
+	}
 
-	public static void main(String[] args) { 
-	    System.out.println("Hello World!"); // Display the string. 
-	} 
-
+	public static int Sub (int aNum1, int aNum2)
+	{
+		return aNum1 - aNum2;
+	}
 }
